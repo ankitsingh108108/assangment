4f70e55ed51efb22d0ff6e97acce35f040941465
Ankit Kumar Singh <ankit.singh@marsdevs.com>
2024-01-19 11:34:28 +0530

    fix alert issue

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import "@/styles/globals.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import type { AppProps } from "next/app";
 import { ToastContainer } from "react-toastify";
+import "react-toastify/ReactToastify.min.css";
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
